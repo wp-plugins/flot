6f574ea6274de4c46d81a13b496d3b346ee02f84
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-01 14:07:59 +0000

    tag 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/flot/tags/1.0.1@1057888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flot.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Flot
+Plugin URI: http://www.ramoonus.nl/wordpress/flot/
+Description: Flot is a pure Javascript plotting library for jQuery. It produces graphical plots of arbitrary datasets on-the-fly client-side.
+Version: 1.0.1
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/
+License: GPL2
+*/
+function rw_flot() {
+		wp_deregister_script('flot'); // deregister
+		wp_enqueue_script('flot', plugins_url('/flot/jquery.flot.min.js', __FILE__), array("jquery", "jquery-ui-core", "excanvas" ), '0.7.0');
+}
+add_action('init', 'rw_flot');
+?>
